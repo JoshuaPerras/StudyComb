b2daa16889a8dff24757cba60a60264e7f9e8f6c
Joshua Perras <joshperras11@gmail.com>
2025-04-08 21:19:36 -0400

    Map and signup added and updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
             <h1>pic</h1>
             <div className="menu">
                 <div className='text'><Link href='/' >Home</Link></div>
-                <div className='text'><Link href='/profile' >profile</Link></div>
-                <div className='text'>filter</div>
-                <div className='text'>sign-in</div>
+                <div className='text'><Link href='/map' >map</Link></div>
+                <div className='text'>filer</div>
+                <div className='text'><Link href='/signup' >sign-up</Link></div>
             </div>
         </div>
     );
