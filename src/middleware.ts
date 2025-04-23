1f9d441e6b8fdb4b19c218e3952c8ef238b16125
Ychoi001 <131182663+Ychoi001@users.noreply.github.com>
2025-04-22 21:28:28 -0400

    middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,23 @@ const { auth } = NextAuth(authConfig);
 
 const middleware = async (request: NextRequest) => {
     const { pathname } = request.nextUrl;
+
+    if (pathname.startsWith("/api/favorites/locations")) {
+      return NextResponse.next();
+    } 
+    
     const session = await auth();
     const isAuthenticated = !!session?.user;
     console.log(isAuthenticated, pathname); 
 
-    const publicPaths = ["/", "/show-item", "/show-items", "/api/items"];
+    const publicPaths = ["/", "/show-item", "/show-items", "/api/items", "/not-found", "/register", "/signup", "/filter", "/login"];
+    const pPaths = ["/", "/show-item", "/not-found", "/show-items", "/api/items", "/profile", "/register", "/review", "/signup", "/map", "/filter", "/login", "/addRoom"];
 
-    if (!isAuthenticated && !publicPaths.includes(pathname)) {
+     if (!pathname.startsWith("/details/") && !pPaths.includes(pathname)) {
+       return NextResponse.redirect(new URL("/not-found", request.url));
+    }
+
+    if (!isAuthenticated && !publicPaths.includes(pathname) && !pathname.startsWith("/details/")) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
@@ -22,9 +32,10 @@ const middleware = async (request: NextRequest) => {
 
 export const config = {
   matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
     "/addRoom/:path*",
-    "/review/:path*"
-  ],
-};
+    "/review/:path*",
+  ]
+}; 
 
 export default middleware;
